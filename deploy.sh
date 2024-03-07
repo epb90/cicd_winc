1b6a28857614c92219e02834be28fdf20c821634
rikwritescode <epbakker90@gmail.com>
2024-03-07 11:51:51 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
-# Navigate to the directory of your application
 cd /home/simple-app || exit
 
-# Pull the latest changes from the main branch of your GitHub repository
 git pull origin main
 
-# Install Python dependencies
 pip install -r requirements.txt
 
-# Restart your application service (replace 'your_app_service' with the actual service name)
-systemctl restart your_app_service
-
-# Verify that the application service is running
 systemctl status your_app_service
